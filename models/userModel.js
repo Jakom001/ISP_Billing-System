ce3bd2271f55ab2ac31809748d46783b8b96d193
jakom001 <andrewpeter2032@gmail.com>
2024-12-05 22:32:43 +0300

    index, usermodels

---
+++ models/userModel.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: string,
+        require: [true, "firstName is required"],
+        trim: true,
+        lowercase: true,
+    },
+    lastName: {
+        type: string,
+        require: [true, "lastName is required"],
+        trim: true,
+        lowercase: true,
+    },
+    username: {
+        type: string,
+        require: [true, "userName is required"],
+        trim: true,
+        lowercase: true,
+    },
+    lastName: {
+        type: string,
+        require: [true, "firstName is required"],
+        trim: true,
+        lowercase: true,
+    },
+    firstName: {
+        type: string,
+        require: [true, "firstName is required"],
+        trim: true,
+        lowercase: true,
+    },
+    lastName: {
+        type: string,
+        require: [true, "firstName is required"],
+        trim: true,
+        lowercase: true,
+    },
+    firstName: {
+        type: string,
+        require: [true, "firstName is required"],
+        trim: true,
+        lowercase: true,
+    },
+    lastName: {
+        type: string,
+        require: [true, "firstName is required"],
+        trim: true,
+        lowercase: true,
+    },
+    firstName: {
+        type: string,
+        require: [true, "firstName is required"],
+        trim: true,
+        lowercase: true,
+    },
+    lastName: {
+        type: string,
+        require: [true, "firstName is required"],
+        trim: true,
+        lowercase: true,
+    },
+    firstName: {
+        type: string,
+        require: [true, "firstName is required"],
+        trim: true,
+        lowercase: true,
+    },
+    lastName: {
+        type: string,
+        require: [true, "firstName is required"],
+        trim: true,
+        lowercase: true,
+    },
+    
+})
